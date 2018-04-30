76cf0cb9c61608c8be1fae3daef9a51e9f285c05
MGRiv <mgrivera501@gmail.com>
2018-04-30 17:44:21 -0400

    Update pivid.py

--- pivid.py
+++ pivid.py
@@ -7,6 +7,6 @@ class capture:
 
     def launch():
         cmd = "gst-launch-1.0 rpicamsrc bitrate=1000000 ! 'video/x-h264,\
-width="+self.width+",height="+self.height+"' ! h264parse ! queue ! rtph264pay config-interval=1 pt=96 ! gdppay ! \
-udpsink host="+self.ip+" port=5000"
+        width="+self.width+",height="+self.height+"' ! h264parse ! queue ! rtph264pay config-interval=1 pt=96 ! gdppay ! \
+        udpsink host="+self.ip+" port=5000"
         os.system(cmd)
