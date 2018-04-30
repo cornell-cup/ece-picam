5eb2daafd4eb102b9dd7b3b1df33051b7b639a23
VAIDEHI GARG <vg254@cornell.edu>
2018-04-30 17:48:16 -0400

    remove slashes from pivid.py

--- pivid.py
+++ pivid.py
@@ -6,7 +6,5 @@ class capture:
         self.ip = ip
 
     def launch():
-        cmd = "gst-launch-1.0 rpicamsrc bitrate=1000000 ! 'video/x-h264,\
-        width="+self.width+",height="+self.height+"' ! h264parse ! queue ! rtph264pay config-interval=1 pt=96 ! gdppay ! \
-        udpsink host="+self.ip+" port=5000"
+        cmd = "gst-launch-1.0 rpicamsrc bitrate=1000000 ! 'video/x-h264, width="+self.width+",height="+self.height+"' ! h264parse ! queue ! rtph264pay config-interval=1 pt=96 ! gdppay ! udpsink host="+self.ip+" port=5000"
         os.system(cmd)
